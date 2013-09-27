120378cca4d18ad709ba748756167d93a805f314
Xander <x@xandervictory.id.au>
2013-09-27 16:53:38 +0800

    add logical address allocation

--- CECNode.js
+++ CECNode.js
@@ -19,14 +19,17 @@ function CECNode(logical_addr) {
     if (arguments.length == 0)
     	throw "must specify logical address!";
     events.EventEmitter.call(this);
-    this.logical_addr = logical_addr;
+    this.logical_addr = ceccodes.CEC_UNREGISTERED;
+    if (arguments.length)
+    	this.logical_addr = logical_addr;
 	this.listened = false;
     var cecnode=this;
    	fs.open('/dev/CEC', 'r+', function(err,fd){
 		cecfd = fd;
 		//console.log('opened '+err);
 		readStream = fs.createReadStream('/dev/CEC', {'fd': fd, autoClose: false, 'flags': 'r+'});
-		cecnode.listen();
+		if (cecnode.logical_addr != ceccodes.CEC_UNREGISTERED)
+			cecnode.listen();
 		cecnode.emit('connected');
 	    readStream.on('readable', function() {
 	  		// there is some data to read now
@@ -99,3 +102,69 @@ function readCecPacket(readStream){
 
 
 CECNode.prototype.listen = function() {/*console.log(this.logical_addr);*/ /*console.log("iocl: %d", */ioctl.setLogicalAddress(cecfd, this.logical_addr)/*)*/; }
+
+CECNode.prototype.ping = function(dst)
+{
+	return this.sendMessage({"dst": dst});
+}
+
+var CECDeviceType = {
+    CEC_DEVICE_INVALID: 0
+    /** TV */
+    CEC_DEVICE_TV: 1,
+    /** Recording Device */
+    CEC_DEVICE_RECODER: 2,
+    /** Tuner */
+    CEC_DEVICE_TUNER: 3,
+    /** Playback Device */
+    CEC_DEVICE_PLAYER: 4,
+    /** Audio System */
+    CEC_DEVICE_AUDIO: 5
+};
+
+var deviceTypeToLogical = [
+	[ CEC_DEVICE_TV, 0 ],
+	[ CEC_DEVICE_TV, 14 ],//E
+	[ CEC_DEVICE_RECODER, 1 ],
+    [ CEC_DEVICE_RECODER, 2 ],
+    [ CEC_DEVICE_TUNER, 3 ],
+    [ CEC_DEVICE_PLAYER, 4 ],
+    [ CEC_DEVICE_AUDIO, 5 ],
+    [ CEC_DEVICE_TUNER, 6 ],
+    [ CEC_DEVICE_TUNER, 7 ],
+    [ CEC_DEVICE_PLAYER, 8 ],
+    [ CEC_DEVICE_RECODER, 9 ],
+    [ CEC_DEVICE_TUNER, 10 ],//A
+    [ CEC_DEVICE_PLAYER, 11 ]//B
+    //13 & 14 are reserved
+];
+
+CECNode.prototype.allocateAddress = function(devicetype, physaddr) {
+	if (arguments.length == 0 || devicetype < 1)
+		devicetype = CEC_DEVICE_PLAYER;
+	var assignedaddr = ceccodes.CEC_UNREGISTERED;
+	for (i=0; i<deviceTypeToLogical.length; i++)
+	{
+		if (deviceTypeToLogical[i][0] == devicetype)
+		{
+			if (this.ping(deviceTypeToLogical[i][1]) == 0)//address is free
+			{
+				assignedaddr = deviceTypeToLogical[i][1];
+				break;
+			}
+		}
+	}
+	if (assignedaddr == ceccodes.CEC_UNREGISTERED && this.ping(14) == 0)//check if free use is available
+		assignedaddr = 14;
+
+	if (assignedaddr==ceccodes.CEC_UNREGISTERED){//if not even the free use addr is available, bail out with the unregistered address
+		return ceccodes.CEC_UNREGISTERED;
+	}
+
+	if (arguments.length == 2)//a physical address was given
+	{
+		this.sendMessage({dst: 15, opcode: ceccodes.CEC_INFO_PHYS_ADDR, args: [physaddr >> 4, physaddr & 0xff, devicetype]});
+	}
+
+	return assignedaddr;
+}
